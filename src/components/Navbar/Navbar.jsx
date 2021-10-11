968a851d56a3325de76b1c2fd574029d0de4d54d
heetkumar <heetkumarpatel@gmail.com>
2021-10-11 23:13:03 +0530

    Navbar of the mobile device created successfully

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import {BiChevronRight} from "react-icons/bi";
+import { IoSearchOutline } from "react-icons/io5";
 
 const NavSm = () => {
     return(
-        <div className="flex">
+        <div className="flex justify-between items-center px-4 py-2">
             <div className="flex-col">
-                <div className="text-xl">It All Starts Here!</div>
-                <div className="text-xl">Ahmedabad</div>
+                <h4 className="text-2xl text-white font-bold">It All Starts Here!</h4>
+                <h4 className="text-xs text-gray-300 flex items-center">Ahmedabad <BiChevronRight /></h4>
             </div>
-            <div className=""></div>
+            <div className="text-white text-2xl "><IoSearchOutline /></div>
         </div>
     );
 };
@@ -17,8 +19,8 @@ const NavLg = () => {};
 const Navbar = () => {
     return (
         <>
-            <nav>
-                <div className="md:hidden">{/*mobile device */}</div>
+            <nav className="bg-navc-50">
+                <div className="md:hidden">{/*mobile device */}<NavSm /></div>
                 <div className="hidden md:flex lg:hidden">{/*medium device */}</div>
                 <div className="hidden lg:flex">{/*Large Device */}</div>
             </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{jsx,js,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navc: {
+          50: "rgb(46, 49, 71)"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
